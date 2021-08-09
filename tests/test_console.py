ce9e514c5364e9e1a99d2f24f4734ce87c67b597
Alfred Tuva <muindetuva@gmail.com>
2021-08-09 17:01:25 +0300

    Add tests for the new create command with parameters

--- tests/test_console.py
+++ tests/test_console.py
@@ -69,3 +69,23 @@ class TestHBNBCommand_create(unittest.TestCase):
                 self.assertIn(new_key, storage.all().keys())
 
 
+class TestHBNBCommand_create_with_parameters(unittest.TestCase):
+    """Test the create command when parameters are passed"""
+
+
+        
+    def test_HBNBCommand_create_new_instances_with_pars(self):
+        """Test creation of new instances with parameters"""
+        Mm = ['BaseModel', 'User', 'Place',
+            'City', 'State', 'Review', 'Amenity']
+
+        for m in Mm:
+            with patch('sys.stdout', new=StringIO()) as f:
+                HBNBCommand().onecmd('''create {}
+                    city_id="0001" user_id="0001" name="My_little_house"
+                    number_rooms=4 number_bathrooms=2 max_guest=10 
+                    price_by_night=300 latitude=37.773972 longitude=-122.431297
+                                     '''.format(m))
+
+                new_key = m + "." + f.getvalue().strip()
+                self.assertIn(new_key, storage.all().keys())

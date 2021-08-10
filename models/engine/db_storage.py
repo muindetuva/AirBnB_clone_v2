63dc5fc9dfd0d204129bc13f85af078361674616
Alfred Tuva <muindetuva@gmail.com>
2021-08-10 14:22:59 +0000

    Add places to the classes list

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -12,7 +12,7 @@ from models.amenity import Amenity
 from models.base_model import Base
 
 
-classes = {"City": City, "State": State, "User": User}
+classes = {"City": City, "State": State, "User": User, "Place": Place}
 
 
 class DBStorage:

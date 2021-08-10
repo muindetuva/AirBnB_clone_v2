cebe3df100377f0e08523b79b1cd62cb68e028d5
Alfred Tuva <muindetuva@gmail.com>
2021-08-10 19:38:18 +0000

    Refactor user

--- models/user.py
+++ models/user.py
@@ -16,19 +16,11 @@ class User(BaseModel, Base):
         first_name(str): User's first name
         last_name(str): User's last name
     """
-    if os.getenv("HBNB_TYPE_STORAGE") == "db":
-        __tablename__ = 'users'
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=False)
-        last_name = Column(String(128), nullable=False)
+    __tablename__ = 'users'
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=False)
+    last_name = Column(String(128), nullable=False)
 
-        places = relationship("Place", backref="user", cascade="all, delete, \
-                              delete-orphan")
-        reviews = relationship("Review", backref="user", cascade="all, delete, \
-                               delete-orphan")
-    else:
-        email = ''
-        password = ''
-        first_name = ''
-        last_name = ''
+    places = relationship("Place", backref="user", cascade="all, delete") 
+    reviews = relationship("Review", backref="user", cascade="all, delete")

9f5c13ea979ec6d40833aa7f0e994e5a7c9d9e14
Alfred Tuva <muindetuva@gmail.com>
2021-08-10 05:36:05 +0000

    Import Base and os

--- models/state.py
+++ models/state.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from models.city import City
+import os
 
-
-class State(BaseModel):
+class State(BaseModel, Base):
     """
     State class
     Attributes:

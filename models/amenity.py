ba3efe58a8362be22ca2108609d17b96d40974a0
Alfred Tuva <muindetuva@gmail.com>
2021-08-10 17:08:52 +0000

    Import place_amenity only when storage is db

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,19 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+import os
+from sqlalchemy.orm import relationship
+if os.getenv("HBNB_TYPE_STORAGE") == "db":
+    from models.place import place_amenity
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """Amenity class"""
+    if os.getenv("HBNB_TYPE_STORAGE") == "db":
+        __tablename__ = 'amenities'
+        name = Column(String(128), nullable=False)
+        place_amenities = relationship("Place", secondary=place_amenity, backref="amenities")
+
+    else:
+        name = ""

f551c7f918bf87a909a12456ed415c67b81bfae9
Alfred Tuva <muindetuva@gmail.com>
2021-08-10 16:54:04 +0000

    Add empty Base class if storage type is not db

--- models/base_model.py
+++ models/base_model.py
@@ -7,8 +7,12 @@ from sqlalchemy import String, Column, DateTime
 from datetime import datetime
 import os
 
+if os.getenv("HBNB_TYPE_STORAGE") == "db":
+    Base = declarative_base()
+else:
+    class Base:
+        pass
 
-Base = declarative_base()
 
 class BaseModel:
     """

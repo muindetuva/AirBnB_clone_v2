bc70ede5aa2894ab474ab6d6d239d8ef668da46c
Alfred Tuva <muindetuva@gmail.com>
2021-08-11 07:39:29 +0000

    Fix errors

--- models/place.py
+++ models/place.py
@@ -34,7 +34,7 @@ class Place(BaseModel, Base):
     if os.getenv("HBNB_TYPE_STORAGE") == "db":
         amenities = relationship('Amenity', secondary=place_amenity,
                                  viewonly=False,
-                                 back_populates='place_amenities')
+                                 back_populates='place')
         reviews = relationship("Review", backref="place", cascade="all,
                                delete")
     else:

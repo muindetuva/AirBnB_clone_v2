af5c313271391b3c0ab5eff65ee793ff09428b5d
Alfred Tuva <muindetuva@gmail.com>
2021-08-11 07:42:39 +0000

    Use string for place_amenity

--- models/amenity.py
+++ models/amenity.py
@@ -4,8 +4,6 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 import os
 from sqlalchemy.orm import relationship
-if os.getenv("HBNB_TYPE_STORAGE") == "db":
-    from models.place import place_amenity
 
 
 class Amenity(BaseModel, Base):
@@ -14,5 +12,5 @@ class Amenity(BaseModel, Base):
     name = Column(String(128), nullable=False)
 
     if os.getenv("HBNB_TYPE_STORAGE") == "db":
-        place_amenities = relationship("Place", secondary=place_amenity,
+        place_amenities = relationship("Place", secondary='place_amenity',
                                        backref="amenities")

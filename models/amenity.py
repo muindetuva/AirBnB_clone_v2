025fd19c0d7f2683ecef74c41c718134618548b9
Alfred Tuva <muindetuva@gmail.com>
2021-08-12 14:27:59 +0000

    Return conditional

--- models/amenity.py
+++ models/amenity.py
@@ -11,5 +11,6 @@ class Amenity(BaseModel, Base):
     __tablename__ = 'amenities'
     name = Column(String(128), nullable=False)
 
-    place_amenities = relationship("Place", secondary='place_amenity',
-                                    back_populates="amenities")
+    if os.getenv('HBNB_TYPE_STORAGE') == "db":
+        place_amenities = relationship("Place", secondary='place_amenity',
+                                        back_populates="amenities")

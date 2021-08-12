bea4b990f671fdc77be9a2925cf86362dcb3913a
Alfred Tuva <muindetuva@gmail.com>
2021-08-12 14:46:46 +0000

    Add backpopulates to place_amenities

--- models/place.py
+++ models/place.py
@@ -34,7 +34,7 @@ class Place(BaseModel, Base):
     amenity_ids = []
     reviews = relationship("Review", backref="place", cascade="all, delete")
     amenities = relationship('Amenity', secondary=place_amenity,
-                                 viewonly=False, backref='places')
+                              viewonly=False, back_populates='place_amenities')
 
     if os.getenv("HBNB_TYPE_STORAGE") != "db":
         @property

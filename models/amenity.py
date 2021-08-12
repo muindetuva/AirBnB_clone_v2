2cf9441f18e71616b11d94bd108a5c90bedf1a86
Alfred Tuva <muindetuva@gmail.com>
2021-08-12 14:24:20 +0000

    Refactor amenity

--- models/amenity.py
+++ models/amenity.py
@@ -11,6 +11,5 @@ class Amenity(BaseModel, Base):
     __tablename__ = 'amenities'
     name = Column(String(128), nullable=False)
 
-    if os.getenv("HBNB_TYPE_STORAGE") == "db":
-        place_amenities = relationship("Place", secondary='place_amenity',
-                                       back_populates="amenities")
+    place_amenities = relationship("Place", secondary='place_amenity',
+                                    back_populates="amenities")

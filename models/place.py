b16d85e63ccb669d6b1ef8f31da57e47977a960e
Alfred Tuva <muindetuva@gmail.com>
2021-08-12 14:08:01 +0000

    Refactor place

--- models/place.py
+++ models/place.py
@@ -35,9 +35,8 @@ class Place(BaseModel, Base):
     amenity_ids = []
 
 
-    if os.getenv("HBNB_TYPE_STORAGE") == "db":
-        reviews = relationship("Review", backref="place", cascade="all, delete")
-        amenities = relationship('Amenity', secondary=place_amenity,
+    reviews = relationship("Review", backref="place", cascade="all, delete")
+    amenities = relationship('Amenity', secondary=place_amenity,
                                  viewonly=False, backref='places')
     else:
         @property

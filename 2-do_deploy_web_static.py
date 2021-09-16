f0ef84c983e1d2eebaddfb82a483b55b9e28c8d0
Alfred Tuva <muindetuva@gmail.com>
2021-09-16 10:56:45 +0000

    Add task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,38 +1,62 @@
 #!/usr/bin/python3
 '''
-Module contains a fab script to distributes an archive to your web servers
+Module contains a fab script to generates a .tgz archive from the contents
+of the web_static
 '''
-from fabric.api import env, run, put
+from fabric.api import local, env, put, run
 import os
+import time
+
+env.user = 'ubuntu'
+env.hosts = ['34.75.214.109', '35.190.185.74']
 
 
-env.hosts = [
-    '34.75.214.109',
-    '35.190.185.74',
-]
+def do_pack():
+    '''
+    A function that generaees a .tgx archive from the web_static folder
+    '''
+    if not os.path.exists('versions'):
+        os.makedirs('versions')
 
-env.user = "ubuntu"
+    filename = time.strftime("%Y%m%d%H%M%S")
+    fullpath = "versions/web_static_{}.tgz".format(filename)
+    try:
+        local("tar -cvzf {} web_static".format(fullpath))
+        return fullpath
+    except:
+        return None
 
 
 def do_deploy(archive_path):
     '''
-    A function that distributes an archive to your web servers
+    Distributes an archive to my webservers
     '''
-    if not os.path.exists(archive_path):
+    if not os.path.isfile(archive_path):
         return False
-    try:
-        up_archive = archive_path.split("/")[-1]
-        ext_archive = "/data/web_static/releases/" + up_archive.split(".")[0]
-        put("{}".format(archive_path), "/tmp")
-        run("mkdir {}".format(ext_archive))
-        run("tar -xzf /tmp/{} -C {}".format(up_archive, ext_archive))
-        run("rm -rf /tmp/{}".format(up_archive))
-        run("mv {}/web_static/* {}/".format(ext_archive, ext_archive))
-        run("rm -rf {}/web_static".format(ext_archive))
 
-        run("rm -rf /data/web_static/current")
-        run("ln -s {} /data/web_static/current".format(ext_archive))
+    # Upload the archive to the server
+    put(archive_path, '/tmp/')
 
-        return True
-    except:
-        return False
+    # Create directory where we will extract the file to
+    file_name = archive_path.split('/')[-1]  # Get file name
+    dir_path = '/data/web_static/releases/{}'.format(file_name.split('.')[0])
+    run('mkdir -p {}'.format(dir_path))
+
+    # Uncompress the archive to the folder
+    server_archive_path = '/tmp/' + file_name  # Path of archive in server
+    run('tar -xzf {} -C {}'.format(server_archive_path, dir_path))
+
+    # Delete the archive from the web server
+    run('rm -rf {}'.format(server_archive_path))
+
+    # Delete the symbolic link
+    run('rm -rf /data/web_static/current')
+
+    # Move the files from web_static to web_static_<number>
+    run('mv {}/web_static/* {}'.format(dir_path, dir_path))
+    run('rm -rf {}/web_static'.format(dir_path))
+
+    # Create a new symbolic link
+    run('ln -s {} /data/web_static/current'.format(dir_path))
+
+    return True
